e10dce3e4ee9bbd3ee68215e772829831d22e942
Karel Alfonso <karel.alfonso@Karels-MacBook-Pro.local>
2017-07-10 08:05:30 +1000

    Fix HBase config. HBase zookeeper requires SSH access

--- hbase/entrypoint.sh
+++ hbase/entrypoint.sh
@@ -20,8 +20,30 @@ srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export JAVA_HOME="${JAVA_HOME:-/usr}"
 
+touch /run/openrc/softlevel
+/etc/init.d/sshd start
+
 # shell breaks and doesn't run zookeeper without this
 mkdir -pv /hbase/logs
+
+if ! [ -f /root/.ssh/authorized_keys ]; then
+    ssh-keygen -t rsa -b 1024 -f /root/.ssh/id_rsa -N ""
+    cp -v /root/.ssh/{id_rsa.pub,authorized_keys}
+    chmod -v 0400 /root/.ssh/authorized_keys
+fi
+
+if ! [ -f /root/.ssh/known_hosts ]; then
+        ssh-keyscan localhost || :
+        ssh-keyscan 0.0.0.0   || :
+fi | tee -a /root/.ssh/known_hosts
+
+hostname=$(hostname -f)
+if ! grep -q "$hostname" /root/.ssh/known_hosts; then
+    ssh-keyscan $hostname || :
+fi | tee -a /root/.ssh/known_hosts
+
+sed -i "s/localhost/$hostname/" /hbase/conf/hbase-site.xml
+sed -i "s/localhost/$hostname/" /hbase/conf/regionservers
 
 # tries to run zookeepers.sh distributed via SSH, run zookeeper manually instead now
 #RUN sed -i 's/# export HBASE_MANAGES_ZK=true/export HBASE_MANAGES_ZK=true/' /hbase/conf/hbase-env.sh
